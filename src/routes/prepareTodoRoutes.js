7d3ad8b2e172c6b4084548334cdc0e1e6e30e7fa
rdx <31172722+rd-xx@users.noreply.github.com>
2023-04-13 16:06:29 +0200

    fix: endpoint & typos

--- src/routes/prepareTodoRoutes.js
+++ src/routes/prepareTodoRoutes.js
@@ -1,24 +1,21 @@
-import fetchPost from "../middleware/fetchTodo.js"
+import fetchTodo from "../middleware/fetchTodo.js"
 import TodoModel from "../db/models/TodoModel.js"
 
 const prepareTodoRoutes = (app) => {
   app.get("/todos", async (_, res) => {
-    const posts = await TodoModel.find()
+    const todos = await TodoModel.find()
 
-    res.send({ result: posts })
+    res.send({ result: todos })
   })
 
-  app.get("/todos/latest", async (_, res) => {
-    const latestPost = await TodoModel.findOne({ isDone: false }, null, {
-      sort: {
-        date: -1,
-      },
-    })
+  app.get("/todos/oldest", async (_, res) => {
+    const todos = await TodoModel.find({ isDone: false })
+    const oldestUnoneTodo = todos.sort((a, b) => a.date - b.date)[0]
 
-    res.send({ result: latestPost })
+    res.send({ result: oldestUnoneTodo || null })
   })
 
-  app.patch("/todos/:id", fetchPost, async (req, res) => {
+  app.patch("/todos/:id", fetchTodo, async (req, res) => {
     const { isDone } = req.body
     const { todo } = req.ctx
 
